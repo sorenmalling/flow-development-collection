be55faa393864be4d3a84569ee3350cf5ba133f5
Soren Malling <sma@webshipper.com>
2020-03-21 23:57:11 +0100

    !!! [TASK] Adjust call to setAccount on token to happen when account is present
    This commit moves the call of setAccount on the active token to
    happen before we now if the password is known.
    
    This is to pass the tested account to the signal to handle on
    the failed authentication account as you can
    with authenticated account

--- Neos.Flow/Classes/Security/Authentication/Provider/PersistedUsernamePasswordProvider.php
+++ Neos.Flow/Classes/Security/Authentication/Provider/PersistedUsernamePasswordProvider.php
@@ -106,9 +106,10 @@ class PersistedUsernamePasswordProvider extends AbstractProvider
             return;
         }
 
+        $authenticationToken->setAccount($account);
+
         if ($this->hashService->validatePassword($password, (string) $account->getCredentialsSource())) {
             $authenticationToken->setAuthenticationStatus(TokenInterface::AUTHENTICATION_SUCCESSFUL);
-            $authenticationToken->setAccount($account);
         }
 
     }
